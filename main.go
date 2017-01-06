726bb45b810e8394190ba86c61a176976453ee19
Doug Safford <doug.safford@yahoo.com>
2017-01-06 16:06:27 -0600

    next commit

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CityHandler(res http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York','Tokyo' 'Eat shit', 'fuck you'}")
+	data, _ := json.Marshal("{'cities':'TOAST','JJJJJJJJJJHEY!!!!!','DUDEEEE','San Francisco, Amsterdam, Berlin, New York','Tokyo' 'Eat shit', 'fuck you'}")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
 }
